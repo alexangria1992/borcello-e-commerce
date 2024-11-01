ed68de065c3f5fa5689f5f2a2b0d361bad4b5a1b
Alexandre Angrignon <alex040892@gmail.com>
2024-11-01 20:48:04 +1300

    clerk authentication

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next';
+import '../globals.css';
+import { ClerkProvider } from '@clerk/nextjs';
+
+export const metadata: Metadata = {
+  title: 'Borcella Admin Dashboard',
+  description: 'Admin Dashboard to manage Borcellas data',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang='en'>
+        <body>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}
